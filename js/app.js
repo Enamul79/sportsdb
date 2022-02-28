c4025d936a83a7fde5ab67abef40a282802b9d8e
Muhammad Enamul Haque <enahaque79@gmail.com>
2022-02-28 15:25:04 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -7,8 +7,76 @@ const allPlayers = () =>{
    // console.log(url);
    fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data.player))
+      .then(data => displayPlayersDetails(data.player))
+
    searchField.value = '';
 }
 
 allPlayers();
+
+const displayPlayersDetails = players =>{
+   console.log(players);
+   for(const player of players){
+      const parent = document.getElementById('players-container');
+      const div = document.createElement('div');
+      div.innerHTML = `
+               <div class="card border p-2">
+                  <div class="pro-pic">
+                     <img class="w-50" src="${player.strThumb}" alt="">
+                  </div>
+                  <h4>Name:${player.strPlayer} </h4>
+                  <h5>Country:${player.strNationality} </h5>
+                  <p>details: </p>
+                  <div class="all-buttons">
+                     <button class="btn btn-danger p-2 mb-2">Delete</button>
+                     <button onclick="Details('${player.idPlayer}')" class="btn btn-success p-2 mb-2">Details</button>
+                     
+                  </div>
+               </div>
+   `;
+   parent.appendChild(div);
+   }
+   // const parent = document.getElementById('players-container');
+   // const div = document.createElement('div');
+   // div.innerHTML = `
+   //             <div class="card border">
+   //                <div class="pro-pic">
+   //                   <img class="w-25" src="" alt="">
+   //                </div>
+   //                <h4>Name: </h4>
+   //                <h5>Country: </h5>
+   //                <p>details: </p>
+   //                <div class="all-buttons">
+   //                   <button class="btn btn-danger p-2 mb-2">Delete</button>
+   //                   <button class="btn btn-success p-2 mb-2">Details</button>
+   //                </div>
+   //             </div>
+   // `;
+   // parent.appendChild(div);
+}
+
+const Details = (idPlayer) =>{
+   // console.log(info);
+   const url =`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${idPlayer}`;
+   fetch(url)
+      .then(response => response.json())
+      .then(data => setDetails(data.players[0]))
+}
+const setDetails = (info) =>{
+   // console.log(info);
+   const detailsContainer = document.getElementById('details-container');
+   const detailsContainerText = detailsContainer.innerHTML;
+   detailsContainer.innerHTML=`
+      <div class="card border">
+
+                  <div class="pro-pic">
+                     <img class="w-50" src="${info.strThumb}" alt="">
+                  </div>
+                  <h4>Name:${info.strPlayer} </h4>
+                  <h5>Country: </h5>
+                  <p>details: </p>
+
+               </div>
+   `;
+   detailsContainerText = '';
+}
